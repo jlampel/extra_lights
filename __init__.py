f9248eb52f4336138d504038e34c3de9c5e0343f
jlampel <jlampel@cgcookie.com>
2020-03-18 17:17:23 -0700

    Update for Eevee
    Added conversions to set the default light properties so that all lights now work with Eevee

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ bl_info = {
 }
 
 import bpy
-from bpy.props import IntProperty
+from bpy.props import IntProperty, BoolProperty
 from bpy.types import Operator, PropertyGroup
 from bpy.utils import register_class, unregister_class
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
@@ -21,17 +21,20 @@ from pathlib import Path
 # set light properties 
 
 class Light:
-    def __init__(self, name, id, lightType, radius, temp, lumens):
+    def __init__(self, name, id, lightType, radius, temp, strength):
         self.name = name
         self.id = id
         self.lightType = lightType
         self.radius = radius
         self.temp = temp
-        self.lumens = lumens
+        self.strength = strength
 
 nat_01 = Light("Candle", "candle", "POINT", 0.015, 1800, 12)
 nat_02 = Light("Fireplace", "fireplace", "POINT", 0.228, 2000, 500)
-natural_lights = [nat_01, nat_02]
+nat_03 = Light("Sunset", "sunset", "SUN", 0.526, 3500, 75)
+nat_04 = Light("Overcast Sun", "overcast", "SUN", 50, 7000, 250)
+nat_05 = Light("Direct Sun", "directsun", "SUN", 0.526, 5250, 1000)
+natural_lights = [nat_01, nat_02, nat_03, nat_04, nat_05]
 
 inc_01 = Light("10w C7 Candelabra", "10wc7", "POINT", 0.015, 2700, 70)
 inc_02 = Light("Vintage Tungsten", "vintage", "POINT", 0.025, 2700, 245)
@@ -61,13 +64,14 @@ flourescent_lights = [fl_01, fl_02]
 all_lights = natural_lights + incandescent_lights + led_lights + flourescent_lights
 
 # Add new light
-def create_light(self, context, light, strength, temp):
+def create_light(self, context, light, strength, temp, useNodes):
     light_data = bpy.data.lights.new(name=light.name, type=light.lightType)
     light_object = bpy.data.objects.new(name=light.name, object_data=light_data)
     bpy.context.collection.objects.link(light_object)
     bpy.ops.object.select_all(action='DESELECT')
     light_object.select_set(state = True)
     context.view_layer.objects.active = light_object
+    light_object.location = bpy.context.scene.cursor.location
 
     # Append Lumens Converter if it's not already in the file
     if (bpy.data.node_groups.find("Lumens Converter") == -1):
@@ -77,8 +81,7 @@ def create_light(self, context, light, strength, temp):
             directory = nodes_directory
         )
     
-    # Create nodes
-    def setup_lumens(): 
+    def setup_nodes(): 
         light_data.energy = 1
         light_data.use_nodes = True
         nodes = light_data.node_tree.nodes
@@ -91,24 +94,63 @@ def create_light(self, context, light, strength, temp):
         lumens_node.inputs[0].default_value = strength
         lumens_node.inputs[1].default_value = temp
     
-    # Set properties 
-    if light.lightType == "SPOT" or "POINT":
-        setup_lumens()
-        light_data.shadow_soft_size = light.radius
-    elif light.lightType == "AREA":
-        setup_lumens()
-        light_data.size = light.radius
-    elif light.lightType == "SUN":
-        light_data.energy = strength
-        light_data.angle = light.radius
+    def convert_kelvin(kelvin):
+        kelvin_table = {
+            # Thanks to Andreas Siess: https://andi-siess.de/rgb-to-color-temperature/
+            1000: [255, 56, 0], 1100: [255, 71, 0], 1200: [255, 83, 0], 1300: [255, 93, 0], 1400: [255, 101, 0], 1500: [255, 109, 0], 1600: [255, 115, 0], 1700: [255, 121, 0], 1800: [255, 126, 0], 1900: [255, 131, 0],
+            2000: [255, 138, 18], 2100: [255, 142, 33], 2200: [255, 147, 44], 2300: [255, 152, 54], 2400: [255, 157, 63], 2500: [255, 161, 72], 2600: [255, 165, 79], 2700: [255, 169, 87], 2800: [255, 173, 94], 2900: [255, 177, 101],
+            3000: [255, 180, 107], 3100: [255, 184, 114], 3200: [255, 187, 120], 3300: [255, 190, 126], 3400: [255, 193, 132], 3500: [255, 196, 137], 3600: [255, 199, 143], 3700: [255, 201, 148], 3800: [255, 204, 153], 3900: [255, 206, 159], 
+            4000: [255, 209, 163], 4100: [255, 211, 168], 4200: [255, 213, 173], 4300: [255, 215, 177], 4400: [255, 217, 182], 4500: [255, 219, 186], 4600: [255, 221, 190], 4700: [255, 223, 194], 4800: [255, 225, 198], 4900: [255, 227, 202],
+            5000: [255, 228, 206], 5100: [255, 230, 210], 5200: [255, 232, 213], 5300: [255, 233, 217], 5400: [255, 235, 220], 5500: [255, 236, 224], 5600: [255, 238, 227], 5700: [255, 239, 230], 5800: [255, 240, 233], 5900: [255, 242, 236],            
+            6000: [255, 243, 239], 6100: [255, 244, 242], 6200: [255, 245, 245], 6300: [255, 246, 247], 6400: [255, 248, 251], 6500: [255, 249, 253], 6600: [254, 249, 255], 6700: [252, 247, 255], 6800: [249, 246, 255], 6900: [247, 245, 255],
+            7000: [245, 243, 255], 7100: [243, 242, 255], 7200: [240, 241, 255], 7300: [239, 240, 255], 7400: [237, 239, 255], 7500: [235, 238, 255], 7600: [233, 237, 255], 7700: [231, 236, 255], 7800: [230, 235, 255], 7900: [228, 234, 255],
+            8000: [227, 233, 255], 8100: [225, 232, 255], 8200: [224, 231, 255], 8300: [222, 230, 255], 8400: [221, 230, 255], 8500: [220, 229, 255], 8600: [218, 229, 255], 8700: [217, 227, 255], 8800: [216, 227, 255], 8900: [215, 226, 255], 
+            9000: [214, 225, 255], 9100: [212, 225, 255], 9200: [211, 224, 255], 9300: [210, 223, 255], 9400: [209, 223, 255], 9500: [208, 222, 255], 9600: [207, 221, 255], 9700: [207, 221, 255], 9800: [206, 220, 255], 9900: [205, 220, 255],
+            10000: [207, 218, 255], 10100: [207, 218, 255], 10200: [206, 217, 255], 10300: [205, 217, 255], 10400: [204, 216, 255], 10500: [204, 216, 255], 10600: [203, 215, 255], 10700: [202, 215, 255], 10800: [202, 214, 255], 10900: [201, 214, 255],
+            11000: [200, 213, 255], 11100: [200, 213, 255], 11200: [199, 212, 255], 11300: [198, 212, 255], 11400: [198, 212, 255], 11500: [197, 211, 255], 11600: [197, 211, 255], 11700: [197, 210, 255], 11800: [196, 210, 255], 11900: [195, 210, 255],
+            12000: [195, 209, 255]
+        }
+        rgb = kelvin_table[round(kelvin, -2)]
+        color = [rgb[0]/255, rgb[1]/255, rgb[2]/255]
+        return color
+
+    def convert_lumens(lumens, rgb):
+        power = lumens / ( (rgb[0] * 145.256) + (rgb[1] * 488.449) + (rgb[2] * 49.2955) )
+        return power
     
-    # Set location and rotation
-    light_object.location = bpy.context.scene.cursor.location
-    if light.lightType == "SPOT":
+    # Set properties 
+    if light.lightType == "POINT":
+        light_data.shadow_soft_size = light.radius
+        if useNodes == True:
+            setup_nodes()
+        else:
+            light_data.color = convert_kelvin(temp)
+            light_data.energy = convert_lumens(strength, light_data.color)
+            
+    elif light.lightType == "SPOT":
         light_object.rotation_euler[0] = 1.5708
+        light_data.shadow_soft_size = light.radius
+        if useNodes == True:
+            setup_nodes()
+        else:
+            light_data.color = convert_kelvin(temp)
+            light_data.energy = convert_lumens(strength, light_data.color)
+            
+    elif light.lightType == "AREA":
+        light_data.size = light.radius
+        if useNodes == True:
+            setup_nodes()
+        else:
+            light_data.color = convert_kelvin(temp)
+            light_data.energy = convert_lumens(strength, light_data.color)
+            
     elif light.lightType == "SUN":
         light_object.rotation_euler[0] = 0.785398
         light_object.rotation_euler[1] = 0.785398
+        light_data.energy = strength
+        light_data.angle = light.radius
+        light_data.color = convert_kelvin(temp)
+        
     
 # Create operators
 def create_light_operator(light):
@@ -122,24 +164,29 @@ def create_light_operator(light):
 
         strength: bpy.props.IntProperty(
             name = "Lumens",
-            default = light.lumens,
+            default = light.strength,
             min = 5,
             max = 20000,
             description = "Amound of percieved light emitted as measured in lumens",
         )
-        
         temp: bpy.props.IntProperty(
             name = "Color Temperature",
             default = light.temp,
             min = 1500,
-            max = 8000,
+            max = 12000,
             description = "Color of the light according to the Kelvin temperature scale",
+        )
+        useNodes: bpy.props.BoolProperty(
+            name = "Use Nodes (Cycles only)",
+            default = False,
+            description = "Create light with nodes so that lumens and color temperature can be changed at any time. Currently only works with Cycles."
         )
 
         def execute(self, context):
-                create_light(self, context, light, self.strength, self.temp)
+                create_light(self, context, light, self.strength, self.temp, self.useNodes)
                 return {'FINISHED'}
     return OBJECT_OT_add_light
+
 natural_operators = [create_light_operator(light) for light in natural_lights]
 incandescent_operators = [create_light_operator(light) for light in incandescent_lights]
 led_operators = [create_light_operator(light) for light in led_lights]
