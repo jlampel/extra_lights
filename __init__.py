7c2630a854c4b782dcc03ed52acacc1782160f05
jlampel <jlampel@cgcookie.com>
2020-03-23 14:19:44 -0700

    Updated Presets
    Lights updated with new presets, sun to be added slightly above the active camera, and added an option to set an RGB color from the undo panel

--- __init__.py
+++ __init__.py
@@ -11,11 +11,34 @@ bl_info = {
 }
 
 import bpy
-from bpy.props import IntProperty, BoolProperty
+from bpy.props import IntProperty, BoolProperty, FloatVectorProperty
 from bpy.types import Operator, PropertyGroup
 from bpy.utils import register_class, unregister_class
 import os
 from pathlib import Path
+
+# Append Lumens Converters if they're not already in the file
+
+if os.path.dirname(os.path.abspath(__file__)) == 'C:\\':
+    nodes_directory = "C:/Users/jonat/Documents/GitHub/extra-lights" + "/real_lights.blend\\NodeTree\\"
+else: 
+    nodes_directory = os.path.dirname(os.path.abspath(__file__)) + "/real_lights.blend\\NodeTree\\"
+
+if (bpy.data.node_groups.find("Lumens Converter") == -1):
+    bpy.ops.wm.append(
+        filename = "Lumens Converter", 
+        directory = nodes_directory
+    )
+if (bpy.data.node_groups.find("Spot Lumens Converter") == -1):
+    bpy.ops.wm.append(
+        filename = "Spot Lumens Converter", 
+        directory = nodes_directory
+    )
+if (bpy.data.node_groups.find("Area Lumens Converter") == -1):
+    bpy.ops.wm.append(
+        filename = "Area Lumens Converter", 
+        directory = nodes_directory
+    )
 
 # set light properties 
 
@@ -35,67 +58,53 @@ nat_04 = Light("Overcast Sun", "overcast", "SUN", 50, 7000, 250)
 nat_05 = Light("Direct Sun", "directsun", "SUN", 0.526, 5250, 1000)
 natural_lights = [nat_01, nat_02, nat_03, nat_04, nat_05]
 
-inc_01 = Light("10w C7 Candelabra", "10wc7", "POINT", 0.015, 2700, 70)
-inc_02 = Light("Vintage Tungsten", "vintage", "POINT", 0.025, 2700, 245)
-inc_03 = Light("25w A15", "25wa15", "POINT", 0.0216, 2700, 210)
-inc_04 = Light("40w A15", "40wa15", "POINT", 0.0216, 2700, 400)
-inc_05 = Light("75w A19", "75wa19", "POINT", 0.03, 2775, 1050)
-inc_06 = Light("150w A21", "150wa21", "POINT", 0.033, 2700, 1050)
-inc_07 = Light("200w A21", "200wa21", "POINT", 0.033, 2700, 3880)
-inc_08 = Light("Halogen Car Headlight", "headlighth", "SPOT", 0.02, 4100, 3000)
-inc_09 = Light("300w PS30", "300wps30", "POINT", 0.038, 2700, 5870)
+inc_01 = Light("70 lm C7 Candelabra Appliance Light", "70c7", "POINT", 0.015, 2700, 70)
+inc_02 = Light("250 lm Vintage Tungsten Decorative Light", "vintage", "POINT", 0.025, 2700, 250)
+inc_03 = Light("200 lm A15 Appliance Light", "200a15", "POINT", 0.0216, 2700, 200)
+inc_04 = Light("400 lm A15 Appliance Light", "400a15", "POINT", 0.0216, 2700, 400)
+inc_05 = Light("1000 lm A19 Lamp Light", "1000a19", "POINT", 0.03, 2700, 1000)
+inc_06 = Light("2700 lm A21 Ceiling Light", "2700a21", "AREA", ["Disk", 0.033], 2700, 2700)
+inc_07 = Light("3000 lm Halogen Car Headlight", "3000headlight", "SPOT", [125, 0.02], 4100, 3000)
+inc_08 = Light("3800 lm A21 Ceiling Light", "3800a21", "AREA", ["DISK", 0.033], 2700, 3800)
+inc_09 = Light("5800 lm PS30 Barn Light", "5800ps30", "AREA", ["DISK", 0.038], 2700, 5800)
 incandescent_lights = [inc_01, inc_02, inc_03, inc_04, inc_05, inc_06, inc_07, inc_08, inc_09]
 
-led_01 = Light("400 Lumen Flashlight", "400flashlight", "SPOT", 0.0075, 2700, 400)
-led_02 = Light("1400 Lumen Flashlight", "1400flashlight", "SPOT", 0.0075, 4000, 1400)
-led_03 = Light("60w A19 White LED", "60wa19", "POINT", 0.034, 3000, 800)
-led_04 = Light("100w A21 Daylight LED", "100wa21", "POINT", 0.034, 5000, 1600)
-led_05 = Light("150w PAR38 White LED", "150wpar38", "POINT", 0.06, 3000, 1400)
-led_06 = Light("250w PAR38 White LED", "250wpar38", "POINT", 0.06, 3000, 2600)
-led_07 = Light("LED Car Headlight", "headlightled", "SPOT", 0.02, 6500, 5000)
-led_08 = Light("350w Outdoor Flood", "350wflood", "AREA", 0.178, 5000, 9100)
-led_lights = [led_01, led_02, led_03, led_04, led_05, led_06, led_07, led_08]
+led_01 = Light("400 lm Flashlight", "400flashlight", "SPOT", [65, 0.0075], 2700, 400)
+led_02 = Light("800 lm A19 Lamp LED", "800a19", "POINT", 0.034, 3000, 800)
+led_03 = Light("1400 lm Flashlight", "1400flashlight", "SPOT", [65, 0.0075], 4000, 1400)
+led_04 = Light("1500 lm PAR38 Outdoor Floodlight", "1400par38", "AREA", ["DISK", 0.06], 3000, 1500)
+led_05 = Light("1600 lm A21 Ceiling LED", "1600a21", "AREA", ["DISK", 0.034], 5000, 1600)
+led_06 = Light("2500 lm Photography LED Bar", "2500bar", "AREA", ["RECTANGLE", 0.03, 0.4], 5500, 2500)
+led_07 = Light("2600 lm PAR38 Outdoor Floodlight", "2600par38", "AREA", ["DISK", 0.06], 3000, 2600)
+led_08 = Light("3350 lm Photography LED Pack", "3350pack", "AREA", ["RECTANGLE", 0.23, 0.178], 5600, 3350)
+led_09 = Light("5000 lm LED Car Headlight", "headlightled", "SPOT", [125, 0.02], 6500, 5000)
+led_10 = Light("9100 lm Industrial LED Wall Pack", "9100flood", "AREA", ["RECTANGLE", 0.23, 0.178], 5000, 9100)
+led_lights = [led_01, led_02, led_03, led_04, led_05, led_06, led_07, led_08, led_09, led_10] 
 
-fl_01 = Light("45w Daylight CFL", "45wcfl", "POINT", 0.03, 5500, 2250)
-fl_02 = Light("85w Daylight CFL", "45wcfl", "POINT", 0.03, 6500, 4500)
-fluorescent_lights = [fl_01, fl_02]
+fl_01 = Light("1500 lm Photography Umbrella", "1500ubmrella", "AREA", ["DISK", 0.838], 5000, 1500)
+fl_02 = Light("2000 lm Photography Softbox", "2000softbox", "AREA", ["SQUARE", 0.61], 5000, 2000)
+fl_03 = Light("2250 lm Photography CFL", "2250cfl", "POINT", 0.03, 5500, 2250)
+fl_04 = Light("2500 lm Fluorescent Tube", "2500tube", "AREA", ["RECTANGLE", 0.05, 1.22], 5000, 2500)
+fl_05 = Light("4500 lm Daylight CFL", "4500cfl", "POINT", 0.03, 6500, 4500)
+fluorescent_lights = [fl_01, fl_02, fl_03, fl_04, fl_05]
 
 all_lights = natural_lights + incandescent_lights + led_lights + fluorescent_lights
 
 # Add new light
-def create_light(self, context, light, strength, temp, useNodes, useSky, spotSize):
+def create_light(self, context, light, colType, temp, tint, useNodes, useSky, spotSize):
     bpy.ops.object.light_add(type=light.lightType)
     bpy.context.active_object.name = light.name
     light_object = bpy.context.active_object
     light_data = light_object.data
 
-    # Append Lumens Converters if it's not already in the file
-    if (bpy.data.node_groups.find("Lumens Converter") == -1):
-        nodes_directory = os.path.dirname(os.path.abspath(__file__)) + "/real_lights.blend\\NodeTree\\"
-        bpy.ops.wm.append(
-            filename = "Lumens Converter", 
-            directory = nodes_directory
-        )
-    if (bpy.data.node_groups.find("Spot Lumens Converter") == -1):
-        nodes_directory = os.path.dirname(os.path.abspath(__file__)) + "/real_lights.blend\\NodeTree\\"
-        bpy.ops.wm.append(
-            filename = "Spot Lumens Converter", 
-            directory = nodes_directory
-        )
-    if (bpy.data.node_groups.find("Area Lumens Converter") == -1):
-        nodes_directory = os.path.dirname(os.path.abspath(__file__)) + "/real_lights.blend\\NodeTree\\"
-        bpy.ops.wm.append(
-            filename = "Area Lumens Converter", 
-            directory = nodes_directory
-        )
-    
     def setup_nodes(): 
         light_data.energy = 1
+        col = [tint[0], tint[1], tint[2], 1.0]
         light_data.use_nodes = True
         nodes = light_data.node_tree.nodes
 
         if light.lightType == "SUN":
-            nodes["Emission"].inputs[1].default_value = strength
+            nodes["Emission"].inputs[1].default_value = light.strength
             k = nodes.new("ShaderNodeBlackbody")
             k.inputs[0].default_value = temp
             light_data.node_tree.links.new(k.outputs[0], nodes["Emission"].inputs[0])
@@ -109,9 +118,19 @@ def create_light(self, context, light, strength, temp, useNodes, useSky, spotSiz
             else:
                 lumens_node.node_tree = bpy.data.node_groups["Lumens Converter"]
             light_data.node_tree.links.new(lumens_node.outputs[0], nodes["Light Output"].inputs[0])
-            lumens_node.inputs[0].default_value = strength
-            lumens_node.inputs[1].default_value = temp
+            lumens_node.inputs[0].default_value = light.strength
+            if colType == "rgb":
+                lumens_node.inputs[1].default_value = 6000
+                lumens_node.inputs[2].default_value = col
+            else:
+                lumens_node.inputs[1].default_value = temp
     
+    def setup_color():
+        if colType == "rgb":
+            light_data.color = tint
+        else: 
+            light_data.color = convert_kelvin(light.temp)
+
     def setup_sky():
         bpy.ops.world.new()
         world = bpy.data.worlds["World"]
@@ -169,34 +188,41 @@ def create_light(self, context, light, strength, temp, useNodes, useSky, spotSiz
         power = lumens / ( (rgb[0] * 145.256) + (rgb[1] * 488.449) + (rgb[2] * 49.2955) )
         return power
     
-    # Set properties 
+    # Set light properties 
     if light.lightType == "POINT":
         light_data.shadow_soft_size = light.radius
         if useNodes == True:
             setup_nodes()
         else:
-            light_data.color = convert_kelvin(temp)
-            light_data.energy = convert_lumens(strength, light_data.color)
+            setup_color()
+            light_data.energy = convert_lumens(light.strength, light_data.color)
             light_data.use_custom_distance = True
             
     elif light.lightType == "SPOT":
         light_object.rotation_euler[0] = 1.5708
-        light_data.shadow_soft_size = light.radius
+        light_data.shadow_soft_size = light.radius[1]
+        light_data.spot_size = spotSize / 57.2957795 
+
         if useNodes == True:
             setup_nodes()
         else:
-            light_data.color = convert_kelvin(temp)
-            light_data.spot_size = spotSize / 57.2957795
-            light_data.energy = convert_lumens(strength, light_data.color) * (360 / spotSize)
+            setup_color()
+            light_data.energy = convert_lumens(light.strength, light_data.color) * (360 / spotSize)
             light_data.use_custom_distance = True
             
     elif light.lightType == "AREA":
-        light_data.size = light.radius
+        light_data.shape = light.radius[0]
+        if light_data.shape == "SQUARE" or "DISK":
+            light_data.size = light.radius[1]
+        elif light_data.shape == "RECTANGLE" or "ELLIPSE":
+            light_data.size = light.radius[1]
+            light_data.size_y = light.radius[2]
+
         if useNodes == True:
             setup_nodes()
         else:
-            light_data.color = convert_kelvin(temp)
-            light_data.energy = convert_lumens(strength, light_data.color) * 1.95
+            setup_color()
+            light_data.energy = convert_lumens(light.strength, light_data.color) * 1.95
             light_data.use_custom_distance = True
             
     elif light.lightType == "SUN":
@@ -207,11 +233,18 @@ def create_light(self, context, light, strength, temp, useNodes, useSky, spotSiz
         else:
             light_object.rotation_euler[0] = 0.785398
             light_object.rotation_euler[1] = 0.785398
+
+        # Set the sun slightly above the camera
+        if bpy.context.scene.camera != None:
+            z = (125/100) * bpy.context.scene.camera.location[2]
+            light_object.location[2] = z
+
         if useNodes == True:
             setup_nodes()
         else: 
-            light_data.energy = strength
-            light_data.color = convert_kelvin(temp)
+            light_data.energy = light.strength
+            light_data.color = convert_kelvin(light.temp)
+            
         if useSky:
             setup_sky()
         
@@ -225,7 +258,7 @@ def create_light_operator(light):
         bl_options = {'REGISTER', 'UNDO'}
         name = light.name
         lightType = light.lightType
-
+        
         if light.lightType == "SUN":
             strength: bpy.props.IntProperty(
                 name = "Irradiance",
@@ -242,10 +275,35 @@ def create_light_operator(light):
                 max = 20000,
                 description = "Amound of percieved light emitted as measured in lumens",
             )
+        if light.lightType == "SPOT":
+            spotSize: bpy.props.IntProperty(
+                name = "Spot Angle",
+                default = light.radius[0],
+                min = 1,
+                max = 180,
+                description = "Setting the spot angle here will keep the total amount of light generated the same regardless of the spot size",
+            )
+        else: 
+            spotSize: bpy.props.IntProperty(
+                name = "Spot Angle",
+                default = 65,
+                min = 1,
+                max = 180,
+                description = "This prop should never appear. It just avoids a float object being unsubscriptable error",
+            )
         useNodes: bpy.props.BoolProperty(
             name = "Use Nodes (Cycles only)",
             default = False,
             description = "Create light with nodes so that lumens and color temperature can be changed at any time. Currently only works with Cycles."
+        )
+        colType: bpy.props.EnumProperty(
+            items = [
+                ("kelvin", "Kelvin Temperature", "Set the light color according to Kelvin values"),
+                ("rgb", "RGB Color", "Set the light color to a specific RGB value")
+            ],
+            name = "Set Color With",
+            description = "Set the light's color with either Kelvin or RGB values",
+            default = "kelvin",
         )
         temp: bpy.props.IntProperty(
             name = "Color Temperature",
@@ -254,26 +312,36 @@ def create_light_operator(light):
             max = 12000,
             description = "Color of the light according to the Kelvin temperature scale",
         )
+        tint: bpy.props.FloatVectorProperty(
+            subtype = 'COLOR',
+            name = "Color Tint",
+            default = (1.0, 1.0, 1.0),
+            description = "Sets the RGB color of the light"
+        )
         useSky: bpy.props.BoolProperty(
             name = "Create Linked Sky (Cylces only)",
             default = False,
             description = "Create a new World with a Sky texture linked to the rotation of the sun lamp. Currently the Sky texture only renderes correctly in Cycles."
         )
-        spotSize: bpy.props.IntProperty(
-            name = "Spot Angle",
-            default = 65,
-            min = 1,
-            max = 180,
-            description = "Setting the spot angle here will keep the total amount of light generated the same regardless of the spot size",
-        )
+
         def draw(self, context):
             layout = self.layout
             layout.use_property_split = True
             layout.use_property_decorate = False
 
             layout.prop(self, "strength")
-            layout.prop(self, "temp")
+            layout.prop(self, "colType")
+
+            if light.lightType == "SUN":
+                layout.prop(self, "temp")
+            else:
+                if self.colType == "kelvin":
+                    layout.prop(self, "temp")
+                else:
+                    layout.prop(self, "tint")
+                
             layout.prop(self, "useNodes")
+
             if light.lightType == "SUN":
                 layout.prop(self, "useSky")
             elif light.lightType == "SPOT":
@@ -281,7 +349,7 @@ def create_light_operator(light):
 
 
         def execute(self, context):
-                create_light(self, context, light, self.strength, self.temp, self.useNodes, self.useSky, self.spotSize)
+                create_light(self, context, light, self.colType, self.temp, self.tint, self.useNodes, self.useSky, self.spotSize)
                 return {'FINISHED'}
     return OBJECT_OT_add_light
 
