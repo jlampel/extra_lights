8135d77bc203f58341b628239018bdc4621b62f7
jlampel <jlampel@cgcookie.com>
2020-04-01 10:41:41 -0700

    Revert "Defaulted to Using Nodes if Using Cycles"
    This reverts commit e9c35379a7c989fa8b128ea2a673125893e689bb.

--- light_classes.py
+++ light_classes.py
@@ -151,9 +151,6 @@ class PointLight:
                 data.shadow_soft_size = self.radius
                 data.use_custom_distance = True
 
-                if bpy.context.scene.render.engine == 'CYCLES':
-                    self.useNodes = True
-
                 if self.useNodes:
                     if bpy.data.node_groups.find("Lumens Converter") == -1:
                         bpy.ops.wm.append(filename = "Lumens Converter", directory = nodes_directory)
@@ -236,9 +233,6 @@ class SpotLight:
                 data.shadow_soft_size = self.radius
                 data.spot_size = self.spotAngle / 57.2957795 
                 data.use_custom_distance = True
-
-                if bpy.context.scene.render.engine == 'CYCLES':
-                    self.useNodes = True
 
                 if self.useNodes:
                     if (bpy.data.node_groups.find("Spot Lumens Converter") == -1):
@@ -329,9 +323,6 @@ class AreaLight:
                 data.size_y = self.size[1]
                 data.use_custom_distance = True
 
-                if bpy.context.scene.render.engine == 'CYCLES':
-                    self.useNodes = True
-
                 if self.useNodes:
                     if (bpy.data.node_groups.find("Lumens Converter") == -1):
                         bpy.ops.wm.append(filename = "Lumens Converter", directory = nodes_directory)
@@ -412,9 +403,6 @@ class SunLight:
                 if bpy.context.scene.camera != None:
                     z = (125/100) * bpy.context.scene.camera.location[2]
                     ob.location[2] = z
-
-                if bpy.context.scene.render.engine == 'CYCLES':
-                    self.useNodes = True
 
                 if self.useNodes:
                     if (bpy.data.node_groups.find("Lumens Converter") == -1):
