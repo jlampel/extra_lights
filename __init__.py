064be3cea6ba17af582cc449bd5ead1e6c4cdd69
jlampel <jlampel@cgcookie.com>
2022-12-06 12:37:25 -0800

    Updated licenses

--- __init__.py
+++ __init__.py
@@ -1,15 +1,25 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+'''
+Copyright (C) 2020-2023 Orange Turbine
+https://orangeturbine.com
+orangeturbine@cgcookie.com
+
+This file is part of Extra Lights, created by Jonathan Lampel. 
+
+All code distributed with this add-on is open source as described below. 
+
+Scattershot is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <https://www.gnu.org/licenses/>.
+'''
 
 bl_info = {
     "name": "Extra Lights",
--- light_presets.py
+++ light_presets.py
@@ -1,3 +1,27 @@
+'''
+Copyright (C) 2020-2023 Orange Turbine
+https://orangeturbine.com
+orangeturbine@cgcookie.com
+
+This file is part of Extra Lights, created by Jonathan Lampel. 
+
+All code distributed with this add-on is open source as described below. 
+
+Scattershot is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <https://www.gnu.org/licenses/>.
+'''
+
+
 import bpy
 from . import light_classes
 PointLight = light_classes.PointLight
